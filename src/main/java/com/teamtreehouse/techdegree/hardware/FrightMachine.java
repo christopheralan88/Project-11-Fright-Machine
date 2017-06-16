90ada0ebcd863c10ebc92db108fa980534a1f21f
christopheralan88 <christopheralan88@gmail.com>
2017-06-16 08:52:42 -0400

    cleaned up comments

--- src/main/java/com/teamtreehouse/techdegree/hardware/FrightMachine.java
+++ src/main/java/com/teamtreehouse/techdegree/hardware/FrightMachine.java
@@ -5,33 +5,27 @@ import com.example.accessory.Horn;
 import com.example.accessory.Strobe;
 import com.example.motion.MotionDetector;
 
-import java.util.Observable;
 
 public class FrightMachine extends MotionDetector {
 
-    private final Horn horn; //accessory
-    private final Strobe strobe; //accessory
-    private final Camera camera; //hardware
-    private final CameraAdapter cameraAdapter;  //TODO: CJ TEST
+    private final Horn horn;
+    private final Strobe strobe;
+    private final Camera camera;
+    private final CameraAdapter cameraAdapter;
 
     public FrightMachine() {
         horn = new Horn();
         strobe = new Strobe();
         camera = new Camera();
-        cameraAdapter = new CameraAdapter(camera);  //TODO: CJ TEST
-        this.addObserver(cameraAdapter);  //TODO: CJ TEST
+        cameraAdapter = new CameraAdapter(camera);
+        this.addObserver(cameraAdapter);
         this.addObserver(horn);
         this.addObserver(strobe);
     }
 
     @Override
     public void onMotionDetected() {
-        // TODO: This is hardcoded, we should make it more extensible!  :(
         setChanged();
         notifyObservers();
-
-        //horn.activate();
-        //strobe.activate();
-        //camera.snapPhotos(5);
     }
 }
